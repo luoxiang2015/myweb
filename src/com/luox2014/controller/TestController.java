a84991c53f8406d1652094f5934f1201ea9b650d
lx <344506825@qq.com>
2016-04-28 15:56:36 +0800

    springmvc jackson list乱码问题

--- src/com/luox2014/controller/TestController.java
+++ src/com/luox2014/controller/TestController.java
@@ -2,6 +2,8 @@ package com.luox2014.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +21,18 @@ public class TestController {
 	@Autowired
 	private TestService testService;
 
-	@RequestMapping(value="/test", method = RequestMethod.GET)
+	@RequestMapping(value="/test", produces ="application/json;charset=UTF-8")
 	@ResponseBody
 	public List<Article> test(){
-		
+		System.out.println(testService.Test().get(0).getArticle_author());
 		return testService.Test();
-		
+		//浏览器乱码 右键点击查看源代码
+	}
+	
+	@RequestMapping(value="/testEncode",produces="text/html;charset=UTF-8")
+	@ResponseBody
+	public String testEncode(){
+		return "中文乱码";
 	}
 	
 	
